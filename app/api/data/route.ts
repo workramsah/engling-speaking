0203c211d9667fd158375678f474846e89290856
workramsah <workramsah@gmail.com>
2026-03-15 15:56:57 +0545

    fix

--- app/api/data/route.ts
+++ app/api/data/route.ts
@@ -3,12 +3,6 @@ import { Prisma } from "@/app/generated/prisma/client";
 import { NextResponse } from "next/server";
 
 export async function GET(){
-    try {
-        const users = await prisma.user.findMany();
+    const users = await prisma.user.findMany();
     return NextResponse.json(users)
-        
-    } catch (error) {
-        return NextResponse.json({error:"thisis errror"})
-    }
-    
 }
