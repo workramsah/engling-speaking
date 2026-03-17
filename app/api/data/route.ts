70eb9302d667a80a70e197f1c67612cfdcfc360b
workramsah <workramsah@gmail.com>
2026-03-17 08:03:24 +0545

    added post api

--- app/api/data/route.ts
+++ app/api/data/route.ts
@@ -1,7 +1,25 @@
 import prisma from "@/lib/prisma";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(){
     const users = await prisma.user.findMany();
     return NextResponse.json(users)
+}
+
+export async function POST(request:NextRequest){
+    try {
+      const body = await request.json()
+const values = await prisma.user.create({
+    data:{
+        age:body.age,
+        name:body.name,
+        point:body.point
+    }
+})
+
+return NextResponse.json(values)  
+    } catch (error) {
+     return NextResponse.json({error:"thisi"})   
+    }
+
 }
